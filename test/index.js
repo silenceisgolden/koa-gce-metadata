de44784f1d3ffe10f0d1e4e6e62dddc3cc534e97
AJ Klein <aj@buzzshift.com>
2015-12-14 14:17:25 -0600

    api change, adding prefix feature

--- test/index.js
+++ test/index.js
@@ -8,17 +8,27 @@ const nock = require('nock');
 const assert = require('chai').assert;
 
 const equiv = {
-  gceMeta: {
-    sshKeys: null,
+  gceMetadata: {
     testing: 'true'
   }
 };
 
-const emptyEquiv = {};
+const emptyEquiv = {
+  gceMetadata: {}
+};
+
+const mixedEquiv = {
+  gceMetadata: {
+    prefix_testing: 'true'
+  }
+};
+
+const host = 'http://metadata.google.internal';
+const url = '/computeMetadata/v1/project/attributes/';
 
 function returnState(ctx, next) {
   return next().then(function() {
-    if( 'statereturn' in ctx.query && ctx.query.statereturn.length > 0 ) {
+    if( 'statereturn' in ctx.query ) {
       ctx.body = ctx.state;
       ctx.status = 200;
     }
@@ -28,10 +38,18 @@ function returnState(ctx, next) {
 describe('koa-gce-metadata test suite', function() {
   describe('main features', function() {
 
-    it('should skip if GCE_CUSTOM_METADATA is not set', function(done) {
+    it('should not error with no values', function(done) {
 
-      delete process.env.GCE_CUSTOM_METADATA;
-      assert(process.env.GCE_CUSTOM_METADATA === undefined);
+      let metaRequest = nock(host)
+        .get(url)
+        .query({
+          recursive: true
+        })
+        .reply(200, {
+          sshKeys: 'a string'
+        }, {
+          eTag: 123456789
+        });
 
       const app = new Koa();
 
@@ -39,81 +57,143 @@ describe('koa-gce-metadata test suite', function() {
       app.use(gceMetadata());
 
       request(app.listen())
-      .get('/?statereturn=1')
+      .get('/?statereturn')
       .expect(200)
       .end((err, res) => {
-        if(err) return done(new Error(err));
-        assert.deepEqual(res.body, emptyEquiv);
+        if(err) return done(err);
+        assert.deepEqual(res.body, emptyEquiv, 'Not exactly equal');
         done();
       });
-    });
-
-    it('should respond error if metadata url bad response', function(done) {
-
-      delete process.env.GCE_CUSTOM_METADATA;
-      process.env.GCE_CUSTOM_METADATA = 1;
-
-      const app = new Koa();
-
-      let badRequest = nock('http://metadata.google.internal')
-        .get('/computeMetadata/v1/project/attributes/?recursive=true')
-        .times(1)
-        .reply(501, {});
-
-      app.use(gceMetadata());
-
-      request(app.listen())
-      .get('/')
-      .expect(501, done)
 
     });
 
-    it('should respond error if metadata url not responsive', function(done) {
+    it('should not error with some values', function(done) {
 
-      delete process.env.GCE_CUSTOM_METADATA;
-      process.env.GCE_CUSTOM_METADATA = 1;
-
-      const app = new Koa();
-
-      app.use(gceMetadata());
-
-      request(app.listen())
-      .get('/')
-      .expect(501, done)
-
-    });
-
-    it('should not error', function(done) {
-
-      delete process.env.GCE_CUSTOM_METADATA;
-      process.env.GCE_CUSTOM_METADATA = 1;
-
-      const app = new Koa();
-
-      let metaRequest = nock('http://metadata.google.internal')
-        .get('/computeMetadata/v1/project/attributes/?recursive=true')
-        .times(2)
+      let metaRequest = nock(host)
+        .get(url)
+        .query({
+          recursive: true
+        })
         .reply(200, {
           sshKeys: 'a string',
           testing: 'true'
+        }, {
+          eTag: 123456789
         });
+
+      const app = new Koa();
 
       app.use(returnState);
       app.use(gceMetadata());
 
       request(app.listen())
-      .get('/?statereturn=1')
+      .get('/?statereturn')
       .expect(200)
       .end((err, res) => {
-        if(err) return done(new Error(err));
-        assert.deepEqual(res.body, equiv);
+        if(err) return done(err);
+        assert.deepEqual(res.body, equiv, 'Not exactly equal');
+        done();
+      });
+
+    });
+
+    it('should not error with some values and a prefix', function(done) {
+
+      let metaRequest = nock(host)
+        .get(url)
+        .query({
+          recursive: true
+        })
+        .reply(200, {
+          sshKeys: 'a string',
+          prefix_testing: 'true'
+        }, {
+          eTag: 123456789
+        });
+
+      const app = new Koa();
+
+      app.use(returnState);
+      app.use(gceMetadata({
+        prefix: 'prefix_'
+      }));
+
+      request(app.listen())
+      .get('/?statereturn')
+      .expect(200)
+      .end((err, res) => {
+        if(err) return done(err);
+        assert.deepEqual(res.body, mixedEquiv, 'Not exactly equal');
+        done();
+      });
+
+    });
+
+    it('should catch error and return empty object when metadata URL not found', function(done) {
+
+      const app = new Koa();
+
+      app.use(returnState);
+      app.use(gceMetadata());
+
+      request(app.listen())
+      .get('/?statereturn')
+      .expect(200)
+      .end((err, res) => {
+        if(err) return done(err);
+        assert.deepEqual(res.body, emptyEquiv, 'Not exactly equal');
+        done();
+      });
+
+    });
+
+    it('should not catch error when metadata URL not found and options.throwOnError === true', function(done) {
+
+      const app = new Koa();
+
+      app.use(returnState);
+      app.use(gceMetadata({
+        throwOnError: true
+      }));
+
+      request(app.listen())
+      .get('/')
+      .expect(501, done)
+
+    });
+
+    it('should use cache when cache is valid', function(done) {
+
+      let metaRequest = nock(host)
+        .get(url)
+        .query({
+          recursive: true
+        })
+        .reply(200, {
+          sshKeys: 'a string',
+          testing: 'true'
+        }, {
+          eTag: 123456789
+        });
+
+      const app = new Koa();
+
+      app.use(returnState);
+      app.use(gceMetadata());
+
+      request(app.listen())
+      .get('/?statereturn')
+      .expect(200)
+      .end((err, res) => {
+        if(err) return done(err);
+        assert.deepEqual(res.body, equiv, 'Not exactly equal');
 
         request(app.listen())
-        .get('/?statereturn=1')
+        .get('/?statereturn')
         .expect(200)
         .end((err, res) => {
-          if(err) return done(new Error(err));
-          assert.deepEqual(res.body, equiv);
+          if(err) return done(err);
+          assert.deepEqual(res.body, equiv, 'Not exactly equal');
           done();
         });
 
